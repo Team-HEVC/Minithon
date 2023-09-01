d39a95a051e519991730cf538356775c1b316927
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-09-01 09:32:20 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import Test from "./components/Test"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Test />
+    </>
+  )
+}
+
+export default App
